c8c3d4cb62a4175e3b3f337521b55ca4bd7426e9
Bennett Levaton <blevaton@email.arizona.edu>
2018-06-22 19:19:14 -0700

    Added timeout for programs that hang when in an infinite loop or other such conditions.

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 # Created by Bennett Levaton
 # Allows for easy running of C# files
 # Arguments filename
+function timeout() { perl -e 'FILE TIMEOUT ERROR IN EXECUTION; exec @ARGV' "$@"; }
+
 javac -cp .:\* *.java
 # add output and error messages
-java -cp .:\* TestRunner >> Output.txt 2>&1
+timeout 10 java -cp .:\* TestRunner >> Output.txt 2>&1
+
 
