b0e89a28dd9b8664fbb3f4581eb5e342bb368004
Bennett Levaton <blevaton@email.arizona.edu>
2018-06-25 12:42:26 -0700

    Added a timestamp and created variables for conversion

--- GithubPuller.sh
+++ GithubPuller.sh
@@ -1,13 +1,22 @@
 # Created by Bennett Levaton
-# Allows for easy running of C# files
-# Arguments filename
+# Allows for easy running of Java files
+echo "Grades Matrix Set" > Output.txt
+echo " " >> Output.txt
+
+PROJECT_NAME=" " #The name of your project before special IDs
+YOUR_PROGRAM=" " #The name of your orginization
+YOUR_TEST_FILE=" "; #The name of the Java file you want tested
 
 while IFS= read -r line; do
     echo $line >> Output.txt
-    git clone https://github.com/YOUR_PROGRAM/PROJECT_NAME-$line.git
+    echo $"---------------------------------------------------" >> Output.txt
+    git clone https://github.com/$YOUR_PROGRAM/$PROJECT_NAME-$line.git
     #Copy Lines needed
-    cp ./set-$line/YOUR_TEST_FILE.java .
+    cp ./$PROJECT_NAME-$line/$YOUR_TEST_FILE.java .
+    cd $PROJECT_NAME-$line
+    git rev-list --format=format:'%ci' --max-count=1 `git rev-parse HEAD`>> ../Output.txt
+    cd ..
+    echo $"---------------------------------------------------" >> Output.txt
     bash run.sh
-    rm -r set-$line
-done < GitHubUserNames.txt
-
+    rm -r $PROJECT_NAME-$line
+done < gitHubID.txt
