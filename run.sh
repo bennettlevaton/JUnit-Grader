157cf9d2985e850aa76f1a70821dcef5af0ed9a1
Bennett Levaton <blevaton@email.arizona.edu>
2018-06-26 10:54:08 -0700

    Fixed comments

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 # Created by Bennett Levaton
-# Allows for easy running of C# files
-# Arguments filename
-function timeout() { perl -e 'alarm shift; exec @ARGV' "$@"; }
+# Allows for easy running of Java Files
+
+function timeout() { perl -e 'alarm shift; exec @ARGV' "$@"; } #Timeout in case of infinite loop
 
 javac -cp .:\* *.java
-# add output and error messages
 timeout 10 java -cp .:\* TestRunner >> Output.txt 2>&1
 
 
